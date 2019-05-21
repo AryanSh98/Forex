c075b3931e9b64f796e6b0b6fd3127a0b9ca5a0b
AryanSh98 <xavier.chekhov@gmail.com>
2019-05-21 12:24:11 +0430

    model file upload

--- Keras/test.py
+++ Keras/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-pred = pd.read_csv('Forex_sample_prediction.csv')
+pred = pd.read_csv('/home/aryan/Source/Forex/Keras/Forex_sample_predictions.csv')
 plt.figure()
 plt.plot(pred['prediction'])
 plt.plot(pred['actual'])
