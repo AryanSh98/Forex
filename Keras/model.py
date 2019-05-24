0007f3c33902a5f2c06b7db37c97d8998dff310b
AryanSh98 <xavier.chekhov@gmail.com>
2019-05-24 12:17:32 +0430

    updated model.py

---
+++ Keras/model.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+def build_model():
+    model = Sequential()
+    model.add(LSTM(256,input_shape=(20,1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam',loss='mse')
+    return model
