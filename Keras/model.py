5a88d047d0474b9ab783d75e57d157abe6781110
AryanSh98 <ryn.shkrlbn@gmail.com>
2019-05-31 12:00:18 +0430

    a

--- Keras/model.py
+++ Keras/model.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 # pylint: enable=import-error
 
-def build_model():
+def build_keras_model():
     model = Sequential()
     model.add(LSTM(256,input_shape=(20,1)))
     model.add(Dense(1))
