76b0adc64e554d8a6383e4d11d0cc86f251c8ad2
AryanSh98 <ryn.shkrlbn@gmail.com>
2019-05-31 11:27:31 +0430

    update prediction path

--- Keras/prediction_test.py
+++ Keras/prediction_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-prediction_path =  ''
+prediction_path =  '/home/aryan/Source/Forex/Keras/Forex_sample_predictions.csv'
 pred = pd.read_csv(prediction_path)
 plt.figure()
 plt.plot(pred['prediction'])
